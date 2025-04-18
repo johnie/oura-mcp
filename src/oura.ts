1833b439c167b489efc2026123284775816961d9
Johnie Hjelm <johnie@hjelm.im>
2025-04-18 18:57:27 +0200

    feat: add GeneralOuraSchema and update Oura class methods

--- src/oura.ts
+++ src/oura.ts
@@ -1,7 +1,19 @@
 import { got, type Got } from 'got';
 import { z } from 'zod';
 
-export class Raindrop {
+export const GeneralOuraSchema = {
+  start_date: z.string().datetime().describe('Start date to fetch data'),
+  end_date: z
+    .string()
+    .datetime()
+    .default(new Date().toISOString())
+    .describe('End date to fetch data'),
+  next_token: z.string().optional().describe('Next token to fetch next page'),
+};
+
+type GeneralOuraOptions = z.infer<z.ZodObject<typeof GeneralOuraSchema>>;
+
+export class Oura {
   got: Got;
 
   constructor(apiKey: string) {
@@ -20,7 +32,23 @@ export class Raindrop {
 
     if (!res.ok) {
       throw new Error(
-        `Failed to get personal info: ${res.statusCode}\n${res.body}`,
+        `Failed to get personal info: ${res.statusCode}\n${res.body}`
+      );
+    }
+
+    return json;
+  }
+
+  async getDailyActivity(searchParams: GeneralOuraOptions) {
+    const request = this.got.get('usercollection/daily_activity', {
+      searchParams,
+    });
+
+    const [res, json] = await Promise.all([request, request.json()]);
+
+    if (!res.ok) {
+      throw new Error(
+        `Failed to get daily activity: ${res.statusCode}\n${res.body}`
       );
     }
 
